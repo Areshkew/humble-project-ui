77f41b8611d73197bd9948339fe68ef21655e0d5
Sebastian Ruiz <sebasrhrh@gmail.com>
2024-05-22 16:26:24 -0500

    Se implementa el borrado de la tarjeta

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject, Observable, throwError, BehaviorSubject } from 'rxjs';
-import { catchError, shareReplay, tap } from 'rxjs/operators'
+import { catchError, shareReplay, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +9,11 @@ import { catchError, shareReplay, tap } from 'rxjs/operators'
 export class PaymentService {
   private addCardSubject = new Subject<any>();
   private getCardsSubject = new Subject<any>();
+  private deleteCardSubject = new Subject<any>();
 
   addCard$ = this.addCardSubject.asObservable();
   getCards$ = this.getCardsSubject.asObservable();
+  deleteCard$ = this.deleteCardSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -20,7 +22,7 @@ export class PaymentService {
       tap(response => this.addCardSubject.next(response)),
       shareReplay(1),
       catchError(error => this.handleError(error, this.addCardSubject))
-    )
+    );
   }
 
   getCards(): Observable<any> {
@@ -28,7 +30,15 @@ export class PaymentService {
       tap(response => this.getCardsSubject.next(response)),
       shareReplay(1),
       catchError(error => this.handleError(error, this.getCardsSubject))
-    )
+    );
+  }
+
+  deleteCard(card_num: string): Observable<any> {
+    return this.http.post(`/api/payment/delete-card/${card_num}`, {}).pipe(
+      tap(response => this.deleteCardSubject.next(response)),
+      shareReplay(1),
+      catchError(error => this.handleError(error, this.deleteCardSubject))
+    );
   }
 
   private handleError(error: any, subject: Subject<any>): Observable<never> {
